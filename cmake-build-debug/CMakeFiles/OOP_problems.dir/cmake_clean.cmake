257eeff4c5028f6c2d389eb666690b74e8618b27
Tariod <mykhailovdmt@gmail.com>
2020-05-17 11:43:28 +0300

    Create project stub

---
+++ cmake-build-debug/CMakeFiles/OOP_problems.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/OOP_problems.dir/main.cpp.o"
+  "OOP_problems"
+  "OOP_problems.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/OOP_problems.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
